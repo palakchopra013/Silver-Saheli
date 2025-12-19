8a005f41a756eb4b4943026bf9e56edce1c3fdaf
Vanisha Chopra <vanishachopra@Vanishas-MacBook-Air.local>
2025-12-19 23:22:55 +0530

    Fix image path for CRA production build

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,10 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-// Components
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 
-// Pages
 import Home from './pages/Home';
 import Trips from './pages/Trips';
 import Family from './pages/Family';
@@ -16,16 +14,18 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/trips" element={<Trips />} />
-          <Route path="/family" element={<Family />} />
-          <Route path="/emergency" element={<Emergency />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/trips" element={<Trips />} />
+            <Route path="/family" element={<Family />} />
+            <Route path="/emergency" element={<Emergency />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
