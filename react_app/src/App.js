4c8730007703b16d89e690aa0e76193c37e3fd96
Vanisha Chopra <vanishachopra@Vanishas-MacBook-Air.local>
2025-12-22 10:52:59 +0530

    Fix App.css import to render layout correctly

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css"; // 🔴 THIS WAS MISSING
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
